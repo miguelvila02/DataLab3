454dc6d90863e21a1babf9ecca82cce1c36f7833
miguelvila02 <a95675@alunos.uminho.pt>
2025-11-17 15:50:37 +0000

    client started

---
+++ client/client.py
@@ -0,0 +1,24 @@
+print("------Welcome to the Premier League Games Predictor!------"
+      "\nIn this application, you will be able to predict"
+      "\nthe outcome of upcoming Premier League matches based on"
+      "\nhistorical data and team performance.")
+
+program_state = 0
+while program_state != -1:
+    if (program_state == 0 or program_state == 1):
+        case = program_state
+        if case == 0:
+            print("\n\nPress 1 to get started or 0 to exit the program.")
+            user_input = input("Choose whisely: ")
+            if user_input == '1':
+                program_state = 1
+
+            elif user_input == '0':
+                print("Thanks for the visit. Goodbye!")
+                program_state = -1
+
+            else:
+                print("Invalid input. Please try again.")
+                program_state = 0
+        elif case == 1:
+            print("Program booting... Please wait.")
